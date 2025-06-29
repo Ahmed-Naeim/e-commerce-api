5a73fe64d3dd5ae1e4f184552530c83b444f3290
Ahmed Naeim <eng.a.naeim@gmail.com>
2025-06-29 23:24:26 +0300

    Remove config.env from repository

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const dbConnection  = () => {
-    mongoose.connect(process.env.DB_URI).then((conn) => {
-        console.log(`MongoDB connected: ${conn.connection.host}`);
-    });
-}
-
-module.exports = dbConnection;
